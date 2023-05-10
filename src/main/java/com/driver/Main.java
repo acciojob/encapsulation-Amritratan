f8e1987f8dcf1118a3a1b581784677318a1ddcba
SnakeGame <amritratan123@gmail.com>
2023-05-10 17:54:28 +0530

    get error after task_4 : name has private access in com.driver.RWOnly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  // TAsk 3
+  public static void main(String[] args) {
+    RWOnly readWrite=new RWOnly();
+    //TAsk 4
+    readWrite.name="xyz";
+    readWrite.roll_no=0000;
+    System.out.println(readWrite.name, readWrite.roll_no);
+    // after that we get error: name has private access in com.driver.RWOnly
+  }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class RWOnly {
+//    Task 2
+    private String name;
+    private int roll_no;
     
 }
